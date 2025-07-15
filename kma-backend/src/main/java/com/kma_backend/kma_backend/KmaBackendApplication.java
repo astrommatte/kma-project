0701480d96c15047319ca9a4278d3965206a7abd
Mattias Åström <astrom.matte@gmail.com>
2025-07-15 20:23:13 +0200

    nya backend

---
+++ kma-backend/src/main/java/com/kma_backend/kma_backend/KmaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kma_backend.kma_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmaBackendApplication.class, args);
+	}
+
+}
