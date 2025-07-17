3d966fd8b412b930a2a930d061e687bdef6c8184
Mattias Åström <astrom.matte@gmail.com>
2025-07-17 13:42:28 +0200

    lagt till en test controller

---
+++ kma-backend/src/main/java/com/kma_backend/kma_backend/test/TestController.java
@@ -0,0 +1,13 @@
+package com.kma_backend.kma_backend.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
